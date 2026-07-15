b79b131ef59d770c76488a086f38cf04bfbc58ea
HoloviyIvan <goloviytest@gmail.com>
2026-07-15 20:27:12 +0300

    Add enemy spawning architecture

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,14 +4,26 @@ using UnityEngine;
 public class Enemy : Character
 {
     private Transform target;
-    private void Start()
+    private Action<Enemy> release;
+
+    public void Initialize(Transform newTarget, Action<Enemy> releaseAction)
     {
-        target = FindFirstObjectByType<Player>().transform;
+        target = newTarget;
+        release = releaseAction;
+        RestoreHealth();
     }
 
     private void Update()
     {
+        if (target == null || IsDead)
+            return;
+
         var direction = (target.position - transform.position).normalized;
         transform.position += direction * moveSpeed * Time.deltaTime;
+    }
+
+    protected override void OnDeath()
+    {
+        release?.Invoke(this);
     }
 } 
